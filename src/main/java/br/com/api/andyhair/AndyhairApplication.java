8bf9c33757dc4a7c93128b54f3c9e2c383645aa9
Jefferson S Santos <jefferson.s.santospop@gmail.com>
2022-07-02 17:22:55 -0300

    primeiro commit

---
+++ src/main/java/br/com/api/andyhair/AndyhairApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.andyhair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndyhairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndyhairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/andyhair/AndyhairApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.andyhair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndyhairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
